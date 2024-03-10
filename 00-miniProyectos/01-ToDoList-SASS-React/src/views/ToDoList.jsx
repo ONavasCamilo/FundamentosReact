ee1e6866587314fa3bf1535af1b880353453ecdb
camilo Navas <navasochoa@hotmail.com>
2024-03-10 18:01:31 -0500

    ToDoList funcional con diseño, v1:D

--- 00-miniProyectos/01-ToDoList-SASS-React/src/views/ToDoList.jsx
+++ 00-miniProyectos/01-ToDoList-SASS-React/src/views/ToDoList.jsx
@@ -1,22 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DB_TAREAS from "../helpers/bd_tareas"
 import { Tareas } from "../components/Tareas";
 
 export default function ToDoList() {
-    const [tarea, setTarea] = useState({
-        nombre: "",
-        descripcion: "",
-        status: true
-    })
+
+    const [tarea, setTarea] = useState(DB_TAREAS)
+    const [nuevaTarea, setNuevaTarea] = useState({ nombre: "", descripcion: "", status: true });
+    const [editingTaskId, setEditingTaskId] = useState(null); // ID de la tarea en modo de edición
+    const [editedTaskName, setEditedTaskName] = useState(""); // Nombre editado de la tarea
+
+
+    const handleTaskNameClick = (taskId, taskName) => {
+      setEditingTaskId(taskId);          // Activamos el modo de edición para la tarea con el ID proporcionado
+      setEditedTaskName(taskName);       // Inicializamos el nombre editado con el nombre actual de la tarea
+    };
+
+    const crearTarea = (data) => {
+      setTarea([
+        ...tarea,
+        data
+      ])
+      setNuevaTarea({ nombre: "", descripcion: "", status: true });
+      console.log(DB_TAREAS)
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        alert(tarea.nombre + tarea.descripcion + tarea.status)
+        crearTarea(nuevaTarea)
     }
 
     const handleChange = (e) => {
-        setTarea({
-            ...tarea,
+        setNuevaTarea({
+            ...nuevaTarea,
             [e.target.name]: e.target.value
         })
     }
@@ -28,11 +43,11 @@ export default function ToDoList() {
         <form onSubmit={handleSubmit} className="ToDoList__cont-1__form">
           <div className="cont-1__form-div">
             <label htmlFor="nombre">Nombra tu tarea:</label>
-            <input onChange={handleChange} value={tarea.nombre} type="text" name="nombre" placeholder="*Campo obligatorio" required />
+            <input onChange={handleChange} value={nuevaTarea.nombre} type="text" name="nombre" placeholder="*Campo obligatorio" required />
           </div>
           <div className="cont-1__form-div">
             <label htmlFor="descripcion">Escribe la descripción:</label>
-            <input onChange={handleChange} value={tarea.descripcion} type="text" name="descripcion" />
+            <input onChange={handleChange} value={nuevaTarea.descripcion} type="text" name="descripcion" />
           </div>
           <div className="cont-1__form-div">
           <input type="submit" />
@@ -41,8 +56,8 @@ export default function ToDoList() {
       </div>
 
       <div className="ToDoList__cont-2">
-        { DB_TAREAS 
-        ? DB_TAREAS.map((item, index) => (<Tareas key={index} nombre={item.nombre} descripcion={item.descripcion} status={item.status}/>))   
+        { tarea.length
+        ? tarea.map((item, index) => (<Tareas key={index} nombre={item.nombre} descripcion={item.descripcion} status={item.status}/>))   
         : <p>No hay tareas...</p> }
       </div>
     </section>
