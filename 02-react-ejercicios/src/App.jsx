0c8754b695a4aacd2e16587d0813ce7c07026241
camilo Navas <navasochoa@hotmail.com>
2024-03-09 23:07:18 -0500

    CRUD Api-Fake parte 1 y 2 / 5 - helpHttp.js

--- 02-react-ejercicios/src/App.jsx
+++ 02-react-ejercicios/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 // import ContactForm from "./components/ContactForm.jsx";
-// import CrudApi from "./components/CrudApi.jsx";
-import CrudApp from "./components/CrudApp.jsx";
+import CrudApi from "./components/CrudApi.jsx";
+// import CrudApp from "./components/CrudApp.jsx";
 // import Modals from "./components/Modals.jsx";
 // import SelectsAnidados from "./components/SelectsAnidados.jsx";
 // import SongSearch from "./components/SongSearch.jsx";
@@ -19,10 +19,10 @@ function App() {
       <hr />
       <SongSearch />
       <hr />
-      <CrudApi />
       <hr />
+       <CrudApp />
     */}
-      <CrudApp /> 
+    <CrudApi />
     </>
   );
 }
