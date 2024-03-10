396bc252123541bb209810745495cef6f8552723
camilo Navas <navasochoa@hotmail.com>
2024-03-10 05:53:36 -0500

    diseño form con sass y primera vista, estado creado y obteniendo datos en Components

--- 00-miniProyectos/01-ToDoList-SASS-React/src/views/ToDoList.jsx
+++ 00-miniProyectos/01-ToDoList-SASS-React/src/views/ToDoList.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
 
 export default function ToDoList() {
-    return(
-        <section className='section__ToDoList'>
-            <div>
-                <h3>Empieza agregando tareas al tablero..</h3>
-                <form>
-                    <div>
-                    <label htmlFor="tarea">Nombre de la tarea</label>
-                    <input type="text" name='tarea' placeholder='*Campo obligatorio'/>
-                    </div>
-                    <div>
-                        <label htmlFor="descripcion">Escribe la descripción</label>
-                        <input type="text" name='descripcion'/>
-                    </div>
-                    <input type="submit" />
-                </form>
-            </div>
+    const [tarea, setTarea] = useState({
+        nombre: "",
+        descripcion: "",
+        status: true
+    })
 
-            <div>
-                <p>No hay tareas</p>
-            </div>
-        </section>
-    )
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const handleChange = (e) => {
+        setTarea({
+            ...tarea,
+            [e.target.name]: e.target.value
+        })
+    }
+
+  return (
+    <section className="section__ToDoList">
+      <div className="ToDoList__cont-1">
+        <h3>Empieza agregando tareas al tablero...</h3>
+        <form onSubmit={handleSubmit} className="ToDoList__cont-1__form">
+          <div className="cont-1__form-div">
+            <label htmlFor="nombre">Nombre de la tarea:</label>
+            <input onChange={handleChange} value={tarea.nombre} type="text" name="nombre" placeholder="*Campo obligatorio" />
+          </div>
+          <div className="cont-1__form-div">
+            <label htmlFor="descripcion">Escribe la descripción:</label>
+            <input onChange={handleChange} value={tarea.description} type="text" name="descripcion" />
+          </div>
+          <div className="cont-1__form-div">
+          <input type="submit" />
+          </div>
+        </form>
+      </div>
+
+      <div className="ToDoList__cont-2">
+        <p>No hay tareas...</p>
+      </div>
+    </section>
+  );
 }
